a97526a4ab4e7897a530d926195f81fa1ad36e23
Yttty <Yttty@users.noreply.github.com>
2015-05-29 10:17:17 +0800

    Create train.cc

---
+++ train.cc
@@ -0,0 +1,40 @@
+/*
+ * File name: train.cc
+ * Author: Yang Tianyi, SYSU
+ * Function: the implemention of function Train
+ * Last edit: 2015-5-29
+ *
+ * Copyright (C) 2015 Yang Tianyi
+ * E-mail: yeah-y@qq.com
+ */ 
+
+#include "data.h"
+
+bool WriteBackSucceed(DocumentFrequency& document_frequency) {
+  ofstream out(kDocumentFrequencyPath, ios::trunc);
+  if (!out.is_open())
+    return false;
+  for (int bin_number = 0; bin_number < kHashNumber; ++bin_number) {
+    for (int i = 0; i < document_frequency.get_data(bin_number).size(); ++i) {
+      out << document_frequency.get_data(bin_number).at(i).word << '\t' 
+          << document_frequency.get_data(bin_number).at(i).frequency << endl;
+    }
+  }
+  return true;
+}
+
+void Train(istream & in) {
+  DocumentFrequency document_frequency(kDocumentFrequencyPath);
+  StopwordsList stopwords_list(kStopwordsListPath);
+  string word;
+  while (in >> word) {
+    if (word == (string)"_end_")
+      break;
+    if (!stopwords_list.is_included(word)) {
+     document_frequency.increase_frequency(word);
+    }
+  }
+  if (!WriteBackSucceed(document_frequency))
+    throw runtime_error("Failed to Update DocumentFrequencyList");
+  cout << "Successfully trained." << endl;
+}/*----Train----*/
