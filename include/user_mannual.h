46b0fa638bcfa0c2885ba1b66f1056d6edcc7824
Yttty <Yttty@users.noreply.github.com>
2015-05-29 10:20:58 +0800

    Create user_mannual.h

---
+++ include/user_mannual.h
@@ -0,0 +1,22 @@
+#ifndef USER_MANNUAL_H 
+#define USER_MANNUAL_H
+
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+class UserMannual {
+public:
+  UserMannual(string file_name);
+  void print_welcome_info();
+  void print_help();
+
+protected:
+
+private:
+  string file_name_;
+  const string version_info = "Keyword Extractor Version 1.0";
+};
+
+#endif
