dc5ae41057b6f52e76348b57fa828bc91606b3bf
Terrill <ruby1996@126.com>
2016-06-05 15:47:49 +0800

    Update email

--- user_mannual.cc
+++ user_mannual.cc
@@ -18,8 +18,8 @@ void UserMannual::print_welcome_info() {
   printf("--------------------About--------------------\n");
   printf("Name:\t\t %s\n", this->version_info.c_str());
   printf("Function:\t This program can extract 2 to 10 keywords from a given text.\n");
-  printf("Author:\t\t Tianyi Yang, SYSU.\n");
-  printf("E-mail:\t\t yeah-y@qq.com\n\n");
+  printf("Author:\t\t Terrill Yang, SYSU.\n");
+  printf("E-mail:\t\t tyyang_cn@foxmail.com\n\n");
 }
 
 void UserMannual::print_help() {
